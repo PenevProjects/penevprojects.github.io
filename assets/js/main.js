0e41359a195a36229faa93a3f3cdffffb1747eac
PenevProjects <kaloyan42x@gmail.com>
2020-07-23 18:20:13 +0100

    23 July info update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+
 function copyText() {
 		var emailField = document.getElementById("email-text");
 		emailField.select();
@@ -267,6 +268,12 @@ function copyText() {
 						// Hide article, main.
 							$article.hide();
 							$main.hide();
+															
+							// Stop all iframes from playing
+							$("iframe").each(function() { 
+								var src= $(this).attr('src');
+								$(this).attr('src',src);  
+							});
 
 						// Show footer, header.
 							$footer.show();
